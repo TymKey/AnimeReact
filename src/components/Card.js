d62c7c31947d6aa3f55ccdb60d82f13f2cb50462
Brayan Cruz <brayan123fcruz@outlook.com>
2021-05-04 18:08:22 -0500

    Componentes con CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import exerciseImg from '../images/logo.png'
-import './styles/Card.css'
+import exerciseImg from '../images/logo.png';
+import './styles/Card.css';
 class Card extends React.Component {
   render() {
     return (
-      <div>
-        <div>
-          <img src={exerciseImg} alt=""/>
-        </div>
-        <div>
-            <h1>Technique Guides</h1>
-            <p>Learn amazing street workout and calisthenics</p>
+      <div className="card mx-auto Anime-Card">
+        <div className="card-body">
+          <div className="row center">
+            <div className="col-6">
+              <img src={exerciseImg} alt="" />
+            </div>
+            <div className="col-6 Anime-Card-Info">
+              <h1>Titulo del Anime</h1>
+              <p>Leve Descripción del anime en especifico</p>
+            </div>
+          </div>
         </div>
       </div>
     );
