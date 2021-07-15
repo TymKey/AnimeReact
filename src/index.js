6f5be04c29d75b81439d0fe16068ce1ad5ddfe0c
TymKey <brayan123fcruz@outlook.com>
2021-07-15 17:16:21 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AnimeYT from './AnimeYT'
-import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
